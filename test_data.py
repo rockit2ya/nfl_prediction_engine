7ce4b0ba79b7536a30d1ea68fe2ca55aa0596ff2
Johnny Rockit2ya <johnny@rockit2ya.com>
2026-03-16 16:00:00 -0400

    chore: remove legacy Super Bowl LX files, superseded by v1.0 engine

--- test_data.py
+++
@@ -1,5 +0,0 @@
-import nfl_data_py as nfl
-# Just try to pull the first 5 rows of the 2025 schedule
-df = nfl.import_schedules([2025]).head()
-print("Connection Successful! Here is the schedule data:")
-print(df[['game_id', 'home_team', 'away_team']])
